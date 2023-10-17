0a5209f93554d8bc4a5ed03bb9a1df314cb3efde
Kevin <geisenhofkevin@gmail.com>
2023-10-17 19:46:09 +0100

    Link form validation updated. Update timestamps will update according to user and link status (DSW A10 done)

--- app/Http/Controllers/CommunityLinkController.php
+++ app/Http/Controllers/CommunityLinkController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Channel;
 use App\Models\CommunityLink;
+use App\Http\Requests\CommunityLinkForm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,7 +15,7 @@ class CommunityLinkController extends Controller
      */
     public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $links = CommunityLink::where('approved', 1)->paginate(25);
+        $links = CommunityLink::where('approved', 1)->latest('updated_at')->paginate(25);
         $channels = Channel::orderBy('title','asc')->get();
         return view('community/index', compact("links", "channels"));
     }
@@ -30,32 +31,24 @@ class CommunityLinkController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    public function store(CommunityLinkForm $request): \Illuminate\Http\RedirectResponse
     {
-        $data = $request->validate([
-            'title' => 'required|max:255',
-            'link' => 'required|unique:community_links|url|max:255',
-            'channel_id' => 'required|exists:channels,id'
-        ]);
+        $data = $request->validated();
 
         $data['user_id'] = Auth::id();
 
         $approved = (bool)Auth::user()->isTrusted();
         $data['approved'] = $approved;
 
-        // $data['channel_id'] = 1;
 
-        CommunityLink::create($data);
+        // Send the flash message:
+        $communityLink = new CommunityLink;
 
-        if($data['approved']){
-            // If the link is approved, we should show a successful message:
-            return back()->with('success', 'Your link has been created and published');
-        }
-        else {
-            // If the link is not approved, we should show a warning message:
-            return back()->with('warning', 'Your link has been created,
-            but you need to wait until our admin approves it to be published');
-        }
+        $linkWasSubmitted = $communityLink->hasAlreadyBeenSubmitted($data['link']);
+        $linkWasApproved = $communityLink->hasAlreadyBeenApproved($data['link']);
+
+        return $communityLink->sendLinkFormResult($data['link'], $linkWasSubmitted,
+        $approved, $linkWasApproved, $data);
     }
 
     /**
--- app/Models/CommunityLink.php
+++ app/Models/CommunityLink.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class CommunityLink extends Model
 {
@@ -21,5 +22,101 @@ class CommunityLink extends Model
     public function channel(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Channel::class, 'channel_id');
+    }
+
+    /**
+     * Gets data of the specified link (if it does exist)
+     */
+    public function getExistingLinkData($link)
+    {
+        if($data = static::where('link', $link)->first()){
+            // The link exists, so we return the data:
+            return $data;
+        }
+        // The link does NOT exist, so we do not return anything:
+        return null;
+    }
+
+    /**
+     * Check if the link has been sent before
+     */
+    public function hasAlreadyBeenSubmitted($link): bool
+    {
+        $exists = static::getExistingLinkData($link);
+        if ($exists) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Check if the link has been approved before
+     */
+    public function hasAlreadyBeenApproved($link): bool
+    {
+        if(static::hasAlreadyBeenSubmitted($link)){
+            // The link exists:
+            if($data = static::getExistingLinkData($link)){
+                // Data for the link are retrieved:
+                if($data['approved'] == 1){
+                    // The link has been approved before:
+                    return true;
+                }
+                // The link has NOT been approved before:
+                return false;
+            }
+            // Data for the link cannot be retrieved:
+            return false;
+        }
+        // The link does NOT exist:
+        return false;
+    }
+
+    /**
+     * Sends a fitting flash message for the link form result
+     */
+    public function sendLinkFormResult($link, $linkWasSubmitted, $userIsTrusted, $linkWasApproved, $data = null):
+    \Illuminate\Http\RedirectResponse
+    {
+        if($linkWasSubmitted){
+            $data = static::getExistingLinkData($link);
+
+            // The link was sent before, so we don't create it and handle the case accordingly:
+            if($userIsTrusted){
+                // If the link was approved before, we should show a warning message:
+                $data->touch(); // We update the link's update timestamp
+                $data->save(); // We save the new link's update timestamp
+                return back()->with('warning', 'This link has already been created and published before.
+                We updated the link\'s update timestamp.');
+            }
+            else {
+                if($linkWasApproved){
+                    // If the link was not approved before, we should show an error message:
+                    return back()->with('error', 'This link has already been created and published');
+                }
+                else {
+                    // If the link was not approved before, we should show a warning message:
+                    $data->touch(); // We update the link's update timestamp
+                    $data->save(); // We save the new link's update timestamp
+                    return back()->with('warning', 'This link has already been created, but not published.
+                    Publication still needs our admin\'s approval. We updated the link\'s update timestamp.');
+                }
+            }
+        }
+        else {
+            // The link is new, so we create it:
+            static::create($data);
+
+            if($userIsTrusted){
+                // If the link is approved, we should show a successful message:
+                return back()->with('success', 'Your link has been created and published.');
+            }
+            else {
+                // If the link is not approved, we should show a warning message:
+                return back()->with('warning', 'Your link has been created,
+                but you need to wait until our admin approves it to be published.');
+            }
+        }
     }
 }
